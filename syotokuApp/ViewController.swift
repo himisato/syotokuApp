75a209899584f21f5415160810763bf87d3501cf
Himi Sato <hiro86biro@gmail.com>
2016-10-15 16:21:11 +0900

    Initial Commit

---
+++ syotokuApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  syotokuApp
+//
+//  Created by Himi Sato on 2016/10/15.
+//  Copyright © 2016 Himi Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
