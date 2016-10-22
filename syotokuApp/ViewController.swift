92a1f80dcdeee582c691a271a83eee3e4d43774b
Himi Sato <hiro86biro@gmail.com>
2016-10-22 21:59:48 +0900

    first commit

--- syotokuApp/ViewController.swift
+++ syotokuApp/ViewController.swift
@@ -20,6 +20,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var syotokuField: UITextField!
+    
+    @IBAction func resultButton(_ sender: UIButton) {
+        let syotoku : Float = Float(self.syotokuField.text!) ?? 0
+        var result : Float = 0
+        if syotoku <= 180{
+            
+            result = syotoku * 0.4
+        }
+        else {
+            
+            result = syotoku * 0.3 + 18
+        }
+        
+        switch syotoku {
+        case (0...180):
+            result = syotoku * 0.4
+        case (180...360):
+            result = syotoku * 0.3 + 18
+        case (360...660):
+            result = syotoku * 0.2 + 54
+        default:
+            ()
+        }
 
+        self.resultLabel.text = "\(result)"
+            
+        
+    }
+
+    @IBOutlet weak var resultLabel: UILabel!
 }
 
